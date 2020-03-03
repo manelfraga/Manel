699f5b002b6fe6d643dcea78e26374d53b44c68c
manelfraga <manel.etc.etc@gmail.com>
2020-03-03 19:06:53 +0100

    Segundo commit.

--- src/GestorFigurasTest.java
+++ src/GestorFigurasTest.java
@@ -1,5 +1,5 @@
 public class GestorFigurasTest {
-    public static void main(String [] args){
+    public static void main(String [] args ){
         Figura[]figura = new Figura[100];
         figura[0]=new Rectangulo(10,5);
         figura[1]=new Círculo(8.5);
@@ -7,8 +7,16 @@ public class GestorFigurasTest {
         figura[3]=new Triángulo(4,6);
 
         double SumaAreas = GestorFiguras.Suma(figura);
+        System.out.println(SumaAreas);
+
+        GestorFiguras.sort(figura);
+        for (int i=0; i<figura.length;i++){
+            System.out.println("Figura:" + figura[i].getClass().getName() +", Área:"+ figura[i].area());
+        }
+        System.out.println("Suma de todas las áreas:" + SumaAreas);
+        }
 
 
     }
-}
+
 
